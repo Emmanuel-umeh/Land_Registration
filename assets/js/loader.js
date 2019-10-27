46dc15a006334fcd966f208c3f961389cf4fcb8c
Emmanuel <Emmanuelsumeh@gmail.com>
2019-10-27 07:38:42 -0700

    work

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -5,6 +5,6 @@ var timer = setInterval(function() {
    $("#mdtimer span").text(sec--);
    if (sec == 0) {
 $("#makingdifferenttimer").delay(1000).fadeIn(1000);
-$("#mdtimer").hide(1000) .fadeOut(fast);}
+$("#mdtimer").hide(1000) .fadeOut();}
 },1000);
 });
